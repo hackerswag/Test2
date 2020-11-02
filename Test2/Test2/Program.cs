331c815699a5107c7cd524c654e50b567574bf45
Emil Eneroth <emen02001@utb.vaxjo.se>
2020-11-02 10:08:54 +0100

    Version 5

--- Test2/Test2/Program.cs
+++ Test2/Test2/Program.cs
@@ -10,10 +10,29 @@ namespace Test2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yo dude");
-            Console.WriteLine("Yodude");
-            Console.WriteLine("Brooo");
-            Console.WriteLine("SWag");
+            bool finished = false;
+            while (!finished)
+            {
+                Console.WriteLine("q to exit");
+                
+                Console.WriteLine("Sida:");
+                double sida = double.Parse(Console.ReadLine());
+                Console.WriteLine("Area: " + Area(sida));
+                string meny = Console.ReadLine();
+                if (meny == "q")
+                    finished = true;
+                else
+                    finished = false;
+            }
+            
+
         }
+        static double Area(double s)
+        {
+            double a = s * s;
+            return a;
+
+        }
+
     }
 }
